0d8cb25900f06ff37191b15a17eb3faa836f2930
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-08-13 00:48:48 +0300

    task0

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'db.postgresql',
+        'USER':'myuser',
+        'PORT': '5432'
     }
 }
 
