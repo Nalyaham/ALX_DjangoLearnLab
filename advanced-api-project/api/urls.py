e18877bf93a7d70346621bebdf0a8448fe4980bc
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-08-12 23:45:33 +0300

    task1

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('books/', views.BookList.as_view()),
     path('books/<int:pk>/', views.BookDetail.as_view()),
     path('books/create/', views.BookCreate.as_view()),
-    path('books/<int:pk>/update/', views.BookUpdate.as_view()),
-    path('books/<int:pk>/delete/', views.BookDelete.as_view()),
+    path('books/update/<int:pk>/', views.BookUpdate.as_view()),
+    path('books/delete/<int:pk>/', views.BookDelete.as_view()),
 ] 
