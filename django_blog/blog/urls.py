15a1c2827a169b5a24bf78b8d84223546414b827
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-08-14 17:50:08 +0300

    task1

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include 
 from . import views
 from django.contrib import admin
-
+from blog.views import LoginView, LogoutView, RegisterView, ProfileView
 #Create patterns here 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'), 
+	path('register/', RegisterView.as_view(), name='register'), 
+	path('profile/', ProfileView.as_view, name = 'profile'),
 ]
