374a77100d4d53aec4855fd761aceeb9d3a73253
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-08-22 18:23:50 +0300

    task 1

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -3,7 +3,7 @@ from accounts.models import CustomUser
 
 class Post(models.Model):
 	author = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
-	title = models.TextField(max_length=100)
+	title = models.CharField()
 	content = models.TextField(max_length=100)
 	created_date = models.DateTimeField(auto_now_add=True)
 	updated_date = models.DateTimeField(auto_now=True)
@@ -11,7 +11,7 @@ class Post(models.Model):
 class Comment(models.Model):
 	User = models.ForeignKey(Post, on_delete = models.CASCADE)
 	Post = models.ForeignKey(Post, on_delete = models.CASCADE)
-	content = models.TextField(max_length=100)
+	content = models.TextField()
 	created_date = models.DateTimeField(auto_now_add=True)
 	updated_date = models.DateTimeField(auto_now=True)
 
