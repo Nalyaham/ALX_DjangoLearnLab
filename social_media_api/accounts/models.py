c37a3121055f24f3e3de087d757eea607331806d
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-08-22 16:26:42 +0300

    task 0

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -5,5 +5,5 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     bio = models.CharField(max_length=200)    
-    profile_photo = models.TextField() 
+    profile_picture = models.ImageField
     followers = models.ManyToManyField('self', symmetrical=False, related_name ='following')
