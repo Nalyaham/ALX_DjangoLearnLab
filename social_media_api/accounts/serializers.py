8107b92cc6b8ba622193859776973b214729cd0e
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-08-23 22:10:47 +0300

    task 2

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -32,3 +32,8 @@ class LoginSerializer(serializers.Serializer):
         if user and user.is_active:
             return user
         raise serializers.ValidationError("Invalid credentials")
+    
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'password', 'bio', 'profile_picture', 'following')
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-from rest_framework import generics, status, permissions
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from .models import CustomUser,  Post
+from rest_framework import permissions
+from .models import  Post
 from rest_framework import viewsets
 from .models import Post, Comment 
-from .serializers import PostSerializer, CommentSerializer, UserSerializer
+from .serializers import PostSerializer, CommentSerializer
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -26,35 +24,3 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = [IsOwnerOrReadOnly, permissions.IsAuthenticated]
     def perform_create(self, serializer):
         serializer.save(user = self.request.user)
-
-class follow_user(generics.GenericAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, user_id):
-        try:
-            user_to_follow = CustomUser.objects.get(id=user_id)
-            if user_to_follow == request.user:
-                return Response({"error": "You cannot follow yourself"}, status=status.HTTP_400_BAD_REQUEST)
-            request.user.following.add(user_to_follow)
-            return Response({"message": "User followed successfully"}, status=status.HTTP_200_OK)
-        except CustomUser.DoesNotExist:
-            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
-
-class unfollow_user(generics.GenericAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, user_id):
-        try:
-            user_to_unfollow = CustomUser.objects.get(id=user_id)
-            if user_to_unfollow == request.user:
-                return Response({"error": "You cannot unfollow yourself"}, status=status.HTTP_400_BAD_REQUEST)
-            if user_to_unfollow not in request.user.following.all():
-                return Response({"error": "You are not following this user"}, status=status.HTTP_400_BAD_REQUEST)
-            request.user.following.remove(user_to_unfollow)
-            return Response({"message": "User unfollowed successfully"}, status=status.HTTP_200_OK)
-        except CustomUser.DoesNotExist:
-            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
